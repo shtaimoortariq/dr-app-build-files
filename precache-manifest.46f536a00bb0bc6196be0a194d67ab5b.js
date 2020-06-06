cc2ec167e3f2f875ecb59b642d00edb4810caa44
shtaimoortariq <taimoor_t@yahoo.com>
2020-06-06 22:15:46 +0500

    first commit

---
+++ precache-manifest.46f536a00bb0bc6196be0a194d67ab5b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7e49a3bd3d2a23e152662a4fa39a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d32d62c0ec0f2129305",
+    "url": "/static/css/2.7eca7cab.chunk.css"
+  },
+  {
+    "revision": "63fea3c7ad91e2e85952",
+    "url": "/static/css/main.11798999.chunk.css"
+  },
+  {
+    "revision": "5d32d62c0ec0f2129305",
+    "url": "/static/js/2.128c5e4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.128c5e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63fea3c7ad91e2e85952",
+    "url": "/static/js/main.141908d5.chunk.js"
+  },
+  {
+    "revision": "df356b5f96907e73e1d4",
+    "url": "/static/js/runtime-main.1812e016.js"
+  },
+  {
+    "revision": "e1096356a73c7db5a5f9e8feda681a58",
+    "url": "/static/media/Shasthoboi.e1096356.svg"
+  },
+  {
+    "revision": "d0fc30856a541539009e73c787a68bc1",
+    "url": "/static/media/Ubuntu-Bold.d0fc3085.ttf"
+  },
+  {
+    "revision": "b3488e8486d2b4e3a0666997f91c7ed9",
+    "url": "/static/media/Ubuntu-Regular.b3488e84.ttf"
+  },
+  {
+    "revision": "cf108b5a1da8ecaa95871b9ce1da678c",
+    "url": "/static/media/allergy.cf108b5a.svg"
+  },
+  {
+    "revision": "91aa3eac76bcba2f030e666eb4d94808",
+    "url": "/static/media/family.91aa3eac.svg"
+  },
+  {
+    "revision": "99ba0b263a90abe0e66755dc768aded1",
+    "url": "/static/media/health.99ba0b26.svg"
+  },
+  {
+    "revision": "ed017e2aec50fb51bdeee9321bf1d032",
+    "url": "/static/media/imaging.ed017e2a.svg"
+  },
+  {
+    "revision": "98c2d4c4355d92a3fc63b11ced780dc4",
+    "url": "/static/media/lifestyle.98c2d4c4.svg"
+  },
+  {
+    "revision": "00e7dda395395be0db824d6155cf0a71",
+    "url": "/static/media/roboto-slab-v7-latin-300.00e7dda3.eot"
+  },
+  {
+    "revision": "fafe68ff4de2c30596a0d84665de712c",
+    "url": "/static/media/roboto-slab-v7-latin-700.fafe68ff.eot"
+  },
+  {
+    "revision": "1e09096ff6a997de7f52c3a7eadf7413",
+    "url": "/static/media/roboto-slab-v7-latin-regular.1e09096f.eot"
+  },
+  {
+    "revision": "1407041692674852b39c08acc0958490",
+    "url": "/static/media/treatment.14070416.svg"
+  }
+]);
